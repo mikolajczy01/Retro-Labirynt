8c3519adb88d5e65d65845f82e3b4e49e9d0270a
mikolajczy01 <mikolajczyzak@gmail.com>
2022-01-17 01:13:20 +0100

    game title

--- main.c
+++ main.c
@@ -7,8 +7,18 @@ void clear_moj()
     system("cls");
 }
 
+void napis_labirynt()
+{
+    printf("   d8888b. d88888b d888888b d8888b.  .d88b.          db       .d8b.  d8888b. d888888b d8888b. db    db d8b   db d888888b\n");
+    printf("   88  `8D 88'     `  88  ' 88  `8D .8P  Y8.         88      d8' `8b 88  `8D   `88'   88  `8D `8b  d8' 888o  88 `  88  '\n");
+    printf("   88oobY' 88ooooo    88    88oobY' 88    88         88      88ooo88 88oooY'    88    88oobY'  `8bd8'  88V8o 88    88  \n");
+    printf("   88`8b   88         88    88`8b   88    88         88      88   88 88   b.    88    88`8b      88    88 V8o88    88   \n");
+    printf("   88 `88. 88.        88    88 `88. `8b  d8'         88booo. 88   88 88   8D   .88.   88 `88.    88    88  V888    88   \n");
+    printf("   88   YD Y88888P    YP    88   YD  `Y88P'          Y88888P YP   YP Y8888P' Y888888P 88   YD    YP    VP   V8P    YP   \n");
+}
+
 int main(){
-    printf("Hello World");
+    napis_labirynt();
     clear_moj;
 }
 
